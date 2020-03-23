024add3638f2c07010305cc7eb473280072d4353
BeataWalo <bw81958@sgh.waw.pl>
2020-03-23 16:40:11 +0100

    exercise 2- done

--- src/pl/waw/sgh/Exercise2.java
+++ src/pl/waw/sgh/Exercise2.java
@@ -3,15 +3,14 @@ package pl.waw.sgh;
 public class Exercise2 {
     static void fibonacci(int n) {
         int x = 0;
-        for(int i = 1; i <= 50 ; i += x) {
+        for(int i = 1; i <= n ; i += x) {
             x += i;
             System.out.println(i);
             System.out.println(x);
         }
     }
-
     //verification
     public static void main(String[] args) {
-        fibonacci(10);
+        fibonacci(50);
     }
 }
