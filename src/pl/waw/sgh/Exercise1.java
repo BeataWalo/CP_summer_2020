f03a7e37b8ece54655802fdd693183e2f2919c14
BeataWalo <bw81958@sgh.waw.pl>
2020-03-23 15:21:07 +0100

    exercise 1- done

---
+++ src/pl/waw/sgh/Exercise1.java
@@ -0,0 +1,17 @@
+package pl.waw.sgh;
+
+public class Exercise1 {
+    static int bowling(int n) {
+        int sum = 0;
+        for(int i = 0; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    //verification
+    public static void main(String[] args) {
+        System.out.println(bowling(5));
+        System.out.println(bowling(3));
+        System.out.println(bowling(7));
+    }
+}
